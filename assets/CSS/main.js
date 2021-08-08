56cc278211c890acf3bf05b88cc056471cee4baf
ngovanbang2001 <62241342+ngovanbang2001@users.noreply.github.com>
2021-08-08 16:51:34 +0700

    first commit

---
+++ assets/CSS/main.js
@@ -0,0 +1,7 @@
+var User=function(firstName,lastName,avatar){
+    this.firstName=firstName;
+    this.lastName=lastName;
+    this.avatar=avatar;
+}
+ var author=new User('Bang','Ngo',avatar);
+ var user=new User('Son','Dang','avatar');
